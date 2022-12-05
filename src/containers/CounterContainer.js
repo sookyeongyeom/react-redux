4c9d397b2a0be60d11f4a5550f9c9f9a27fcd51d
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-06 01:15:59 +0900

    Feat: 액션 생성 함수로 액션 객체를 만들어 디스패치

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
 // connect = 컴포넌트와 리덕스를 연동하기 위해 react-redux에서 제공하는 함수
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
 	return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
@@ -13,12 +14,11 @@ const mapStateToProps = (state) => ({
 
 // 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위한 함수
 const mapDispatchToProps = (dispatch) => ({
-	// 임시 함수
 	increase: () => {
-		console.log('increase');
+		dispatch(increase());
 	},
 	decrease: () => {
-		console.log('decrease');
+		dispatch(decrease());
 	},
 });
 
