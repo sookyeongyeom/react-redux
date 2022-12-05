32934c19ef48baf0125b052496624a5ef660ac8d
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-06 01:19:21 +0900

    Refactor: mapStateToProps와 mapDispatchToProps를 connect 함수 내부에 익명 함수 형태로 선언

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -8,20 +8,26 @@ const CounterContainer = ({ number, increase, decrease }) => {
 };
 
 // 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위한 함수
-const mapStateToProps = (state) => ({
-	number: state.counter.number,
-});
+// const mapStateToProps = (state) => ({
+// 	number: state.counter.number,
+// });
 
 // 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위한 함수
-const mapDispatchToProps = (dispatch) => ({
-	increase: () => {
-		dispatch(increase());
-	},
-	decrease: () => {
-		dispatch(decrease());
-	},
-});
+// const mapDispatchToProps = (dispatch) => ({
+// 	increase: () => {
+// 		dispatch(increase());
+// 	},
+// 	decrease: () => {
+// 		dispatch(decrease());
+// 	},
+// });
 
 // connect에 mapStateToProps와 mapDispatchToProps를 넘겨주면 반환되는 함수에
 // 컴포넌트를 전달해주면 리덕스와 연동된 컴포넌트가 만들어짐
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+	(state) => ({ number: state.counter.number }),
+	(dispatch) => ({
+		increase: () => dispatch(increase()),
+		decrease: () => dispatch(decrease()),
+	}),
+)(CounterContainer);
