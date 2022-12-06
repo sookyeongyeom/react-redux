a5f2189f080d90637ce3ec39258e9a3ec2a323d3
sookyeongyeom <sookyeong.dev@gmail.com>
2022-12-07 04:20:19 +0900

    Refactor: useActions 사용

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -2,17 +2,23 @@ import { useSelector, useDispatch } from 'react-redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import { useCallback } from 'react';
+import useActions from '../hooks/useActions';
 
 const TodosContainer = () => {
 	const { input, todos } = useSelector(({ todos }) => ({
 		input: todos.input,
 		todos: todos.todos,
 	}));
-	const dispatch = useDispatch();
-	const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
-	const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-	const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-	const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+	// const dispatch = useDispatch();
+	// const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
+	// const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+	// const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+	// const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+	// 디스패치 함수를 자동으로 생성해주는 useActions 훅으로 대체
+	const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+		[changeInput, insert, toggle, remove],
+		[],
+	);
 
 	return (
 		<Todos
